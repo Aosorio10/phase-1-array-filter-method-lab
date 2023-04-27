7f3528e463fb18f197d0c0ee6f24d37572cf4bfb
Alejandro Osorio <avot3110@hotmail.com>
2023-04-27 12:06:28 -0400

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+
+
+function findMatching(drivers, query) {
+    const queryLower = query.toLowerCase();
+    return drivers.filter((driver) => driver.toLowerCase().includes(queryLower));
+  }
+  
+
+  function fuzzyMatch(drivers, query) {
+    const queryLower = query.toLowerCase();
+    return drivers.filter((driver) => driver.toLowerCase().startsWith(queryLower));
+  }
+  
+
+  function matchName(drivers, query) {
+    const queryLower = query.toLowerCase();
+    return drivers.filter((driver) => driver.name.toLowerCase().includes(queryLower));
+  }
